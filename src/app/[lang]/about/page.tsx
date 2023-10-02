1f3a8fd6cfb53a9689c4bad7e27e8572b7aa5687
Gabriel Lennon <gabriellennon7@gmail.com>
2023-10-02 15:13:00 -0300

    add new pages and configs

---
+++ src/app/[lang]/about/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+import womanCellphoneImage from '@/assets/img/screenlink.png';
+import emailImage from '@/assets/img/email.png';
+import { Footer } from '@/components/Footer';
+import { Locale } from '@/config/i18n.config';
+import { getDictionaryServerOnly } from '@/dictionaries/default-dictionary-server-only';
+import Image from 'next/image';
+import { Mail } from 'lucide-react';
+
+export default function About ({ params }: { params: { lang: Locale }}) {
+    const dict = getDictionaryServerOnly(params.lang)
+
+    return (
+        <main className="h-screen bg-blue-200 flex flex-col justify-between">
+            <div className="py-16">  
+                <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                    <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                        <div className="md:5/12 lg:w-5/12">
+                        <Image 
+                            src={womanCellphoneImage}
+                            alt="image" 
+                            loading="lazy" 
+                            width={500} 
+                            height={500}
+                        />
+                        </div>
+                        <div className="md:7/12 lg:w-6/12">
+                            <h2 className="text-2xl text-blue-500 font-bold md:text-4xl">{dict.about.title}</h2>
+                            <p className="mt-6 text-gray-600">
+                                {dict.about.description}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="container m-auto px-6 py-6 text-gray-600 border-t-2 border-blue-500 md:px-12 xl:px-6">
+                    <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12 flex-row-reverse">
+                        <div className="md:5/12 lg:w-5/12">
+                            <Image 
+                                src={emailImage}
+                                alt="image" 
+                                loading="lazy" 
+                                width={500} 
+                                height={500}
+                            />
+                        </div>
+                        <div className="md:7/12 lg:w-6/12">
+                            <h2 className="text-2xl text-blue-500 font-bold md:text-4xl">{dict.contact.title}</h2>
+                            <p className="mt-6 text-gray-600">
+                                {dict.contact.description}
+                            </p>
+                            <div className='flex items-center gap-2 mt-2'>
+                                <Mail color='#004E87' size={14} />
+                                <p className="text-blue-500 font-medium">
+                                    gabriellennon7@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </main>
+    )
+}
--- src/dictionaries/default-language-collections/default-pt-BR.ts
+++ src/dictionaries/default-language-collections/default-pt-BR.ts
@@ -21,5 +21,31 @@ export const ptBR = {
         successShort: 'Link criado e copiado! 🤩',
         errorShort: 'Ops! Algo deu errado, tente novamente. 😵‍💫',
         invalidUrl: 'Digite uma URL válida!'
+    },
+    privacy: {
+        title: "ICutli Política de Privacidade",
+        description: "Esta Política de Privacidade descreve como o nosso site de encurtador de links coleta, usa e compartilha informações pessoais dos usuários. É importante entender como suas informações são tratadas e protegidas ao usar nosso serviço. Ao acessar e utilizar nosso site, você concorda com as práticas descritas nesta Política de Privacidade.",
+        sectionCollect: "Coleta de Informações",
+        descriptionCollect: "Assim como a maioria dos sites, coletamos automaticamente informações de registro do seu navegador, como seu endereço IP, navegador, sistema operacional, páginas visitadas, duração da visita e outros dados relacionados à navegação. Utilizamos cookies para melhorar a experiência do usuário e coletar informações para análise. Cookies são pequenos arquivos de texto armazenados no seu dispositivo. Terceiros, incluindo o Google, usam cookies para veicular anúncios com base em visitas anteriores do usuário ao nosso website ou a outros websites. O Google e seus parceiros podem veicular anúncios com base nas visitas feitas ao nosso site e/ou a outros sites na Internet",
+        sectionInfoUsage: "Uso das Informações",
+        descriptionInfoUsage: "Análise de Dados: As informações coletadas, incluindo dados de cookies, são usadas para análise, melhorias no site, acompanhamento de métricas de uso e personalização da experiência do usuário. Publicidade: Terceiros, como o Google, usam cookies de publicidade para veicular anúncios com base nas visitas anteriores do usuário ao nosso site e a outros sites na Internet.",
+        sectionOptionsUser: "Opções do Usuário",
+        descriptionOptionsUser: "Desativação de Cookies: Os usuários podem desativar a publicidade personalizada acessando as Configurações de anúncios. Alternativamente, podem visitar o site www.aboutads.info para desativar o uso de cookies de publicidade personalizada de terceiros.",
+        sectionShareInfo: "Compartilhamento de Informações",
+        descriptionShareInfo: "Não compartilhamos informações pessoais identificáveis com terceiros, exceto quando exigido por lei ou com seu consentimento explícito.",
+        sectionDataSecurity: "Segurança de Dados",
+        descriptionDataSecurity: "Tomamos medidas razoáveis para proteger suas informações pessoais contra acesso não autorizado, divulgação, alteração ou destruição. No entanto, nenhum sistema é completamente seguro, e não podemos garantir a segurança absoluta dos dados.",
+        sectionUpdatePolicy: "Alterações na Política de Privacidade",
+        descriptionUpdatePolicy: "Podemos atualizar esta Política de Privacidade de tempos em tempos. Quaisquer alterações significativas serão comunicadas por meio do site ou por outros meios.",
+        sectionQuestions: "Contato",
+        descriptionQuestions: "Se você tiver dúvidas ou preocupações sobre esta Política de Privacidade, entre em contato conosco pelo e-mail: gabriellennon7@gmail.com para obter esclarecimentos. Esta Política de Privacidade foi atualizada pela última vez em 02/10/2023. Lembre-se de que, ao continuar a usar nosso site, você concorda com os termos desta Política de Privacidade. É importante revisar esta política regularmente para se manter informado sobre como suas informações são tratadas.",
+    },
+    about: {
+        title: "ICut.li, o que é?",
+        description: "O ICut.li é uma ferramenta gratuita e rápida que tem o objetivo de te fornecer o seu link encurtado da maneira mais segura e rápida.Suas URLs encurtadas podem ser usadas em publicações, blogs, fóruns, emails, mensagens instantâneas e outros locais."
+    },
+    contact: {
+        title: "Deseja falar conosco?",
+        description: "Deseja entrar em contato conosco? Mande um email e estaremos respondendo em no máximo 24h para dúvidas, sugestões ou relatar algum problema. Estamos sempre disponíveis para você 😁"
     }
 }
