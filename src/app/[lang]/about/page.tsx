eadfe91d4e26c43f799e603273046e0ab92755b1
Gabriel Lennon <gabriellennon7@gmail.com>
2023-10-02 15:50:58 -0300

    back button

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -5,13 +5,20 @@ import { Footer } from '@/components/Footer';
 import { Locale } from '@/config/i18n.config';
 import { getDictionaryServerOnly } from '@/dictionaries/default-dictionary-server-only';
 import Image from 'next/image';
-import { Mail } from 'lucide-react';
+import { ChevronLeftCircle, Mail } from 'lucide-react';
+import Link from 'next/link';
 
 export default function About ({ params }: { params: { lang: Locale }}) {
     const dict = getDictionaryServerOnly(params.lang)
 
     return (
         <main className="h-screen bg-blue-200 flex flex-col justify-between">
+            <div className='flex gap-5 items-center justify-start w-full p-2'>
+                <Link href="/" className='flex items-center gap-2 text-sm font-medium'>
+                    <ChevronLeftCircle color='#50555A' />
+                    Voltar para a Home
+                </Link>
+            </div>
             <div className="py-16">  
                 <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                     <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
